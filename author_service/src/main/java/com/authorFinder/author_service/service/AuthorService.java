e49d724c3ae65d4bfbdcd91bd0b2269ae92cc5f0
dkg2395 <dkg.2395.com>
2024-09-28 20:09:41 +0530

    author finder

---
+++ author_service/src/main/java/com/authorFinder/author_service/service/AuthorService.java
@@ -0,0 +1,22 @@
+package com.authorFinder.author_service.service;
+
+import com.authorFinder.author_service.entity.ApiResponse;
+import com.authorFinder.author_service.entity.Author;
+import lombok.val;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface AuthorService {
+
+    ApiResponse fetchAuthor(String name);
+
+    Author addAuthor(Author author);
+
+    Author getAuthorById(int id);
+
+    List<Author> getAllAuthor();
+
+    Author updateAuthor(Author author, int id);
+
+}
